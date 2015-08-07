81f36e54789d0b37df7a7eecf1298412e399da8e
Kamil Niemczyk <kamil.niemczyk@capgemini.com>
2015-08-07 10:37:35 +0200

    Calling test method

--- xUnitMojeTesty/test/tests.py
+++ xUnitMojeTesty/test/tests.py
@@ -3,13 +3,25 @@ Created on 7 sie 2015
 
 @author: KNIEMCZY
 '''
-class WasRun:
+class TestCase:
+    def __init__(self,name):
+        self.name = name
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+class WasRun(TestCase):
     def __init__(self,name):
         self.wasRun = None
+        TestCase.__init__(self, name)
     def testMethod(self):
         self.wasRun = True
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.testMethod()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+        
+TestCaseTest("testRunning").run()
