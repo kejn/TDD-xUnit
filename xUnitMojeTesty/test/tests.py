84101082ae67e94e5391761343038219950b8eee
Kamil Niemczyk <kamil.niemczyk@capgemini.com>
2015-08-07 11:32:00 +0200

    Call tearDown() at end (@After)

--- xUnitMojeTesty/test/tests.py
+++ xUnitMojeTesty/test/tests.py
@@ -8,21 +8,27 @@ class TestCase:
         self.name = name
     def setUp(self):
         pass
-    def run(self):
+    def tearDown(self):
+        pass
+    def run(self):#,restult):
+#         result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 class WasRun(TestCase):
     def __init__(self,name):
         TestCase.__init__(self, name)
     def testMethod(self):
-        self.wasRun = True
+#         self.wasRun = True
         self.log = self.log + "testMethod "
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = True
+#         self.wasRun = None
+#         self.wasSetUp = True
         self.log = "setUp "
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 class TestCaseTest(TestCase):
     def setUp(self):
@@ -33,7 +39,7 @@ class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         self.test = WasRun("testMethod")
         self.test.run()
-        assert("setUp testMethod " == self.test.log)
+        assert("setUp testMethod tearDown " == self.test.log)
         
 TestCaseTest("testTemplateMethod").run()
 # TestCaseTest("testRunning").run()
