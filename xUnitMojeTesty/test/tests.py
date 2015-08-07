4d5b5b375cc0417322e5c01b112f07077101d510
Kamil Niemczyk <kamil.niemczyk@capgemini.com>
2015-08-07 11:05:02 +0200

    Call setUp at beginning (@Before)

--- xUnitMojeTesty/test/tests.py
+++ xUnitMojeTesty/test/tests.py
@@ -6,22 +6,31 @@ Created on 7 sie 2015
 class TestCase:
     def __init__(self,name):
         self.name = name
+    def setUp(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 class WasRun(TestCase):
     def __init__(self,name):
-        self.wasRun = None
         TestCase.__init__(self, name)
     def testMethod(self):
         self.wasRun = True
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = True
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
         
+TestCaseTest("testSetUp").run()
 TestCaseTest("testRunning").run()
